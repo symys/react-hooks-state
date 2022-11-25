78221d681783ad66b55aa7e0be8ef6256e51be66
SeymaDemir <seymademir35@gmail.com>
2022-11-25 18:12:02 +0300

    tried hook

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,13 +1,20 @@
 import React, { useState } from "react";
 
 function Counter() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(2);
 
   function increment() {
     setCount(count + 1);
   }
 
-  return <button onClick={increment}>I have been clicked {count} times</button>;
+  function decrement() {
+    setCount(count - 1);
+  }
+
+  return <div>
+    <button onClick={increment}>I have been clicked {count} times</button>
+    <button onClick={decrement}>I have been clicked {count} times</button>
+    </div>
 }
 
 export default Counter;
